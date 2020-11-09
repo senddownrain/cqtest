6e8475b082737814a62405c5e68a0e6ff413b7fd
unknown <senddownrain@gmail.com>
2020-11-09 23:06:34 +0300

    template

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCLJtxu9azWLZDJ6oZVJhnN4SQFajuIpZ4",
+    authDomain: "caquites-f4ff5.firebaseapp.com",
+    databaseURL: "https://caquites-f4ff5.firebaseio.com",
+    projectId: "caquites-f4ff5",
+    storageBucket: "caquites-f4ff5.appspot.com",
+    messagingSenderId: "791771999315",
+    appId: "1:791771999315:web:4f76f600c4f15ee46d6aa6",
+    measurementId: "G-9721QDY88X"
+}
--- src/main.js
+++ src/main.js
@@ -2,11 +2,44 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+
+import firebaseConfig from './config/firebase'
+import firebase from 'firebase'
+import 'firebase/firestore'
+import VuetifyConfirm from 'vuetify-confirm'
+import { setupBus } from './infrastructure/eventBus'
+
+const moment = require('moment')
+require('moment/locale/ru')
+
+Vue.use(require('vue-moment'), {
+    moment
+})
+
+Vue.use(VuetifyConfirm, {
+  vuetify,
+  buttonTrueText: 'Да',
+  buttonFalseText: 'Нет',
+  color: 'warning',
+  icon: 'warning',
+  title: 'Опасно',
+  width: 350,
+  property: '$confirm'
+})
+
+const firebaseApp =  firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+
+Vue.$db = db
+
+setupBus()
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
